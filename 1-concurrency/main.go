c3f8beb48a28620a5edf50b0c3784c44a247b8ba
Maksim Dubakin <maxdubakin@yandex.ru>
2025-01-16 11:45:12 +0400

    move magic numbers to constansts

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -7,6 +7,11 @@ import (
 	"time"
 )
 
+const (
+	countRandomNum = 10
+	maxRandomNum   = 100
+)
+
 func main() {
 	// G1 генерирует 10 чисел от 0 до 100 и отправляет по одному числу в канал numCh
 	// G2 получает числа из numCh, считает квадрат числа и отправить в канал squareCh
@@ -18,8 +23,8 @@ func main() {
 	go func() {
 		defer wg.Done()
 		r := rand.New(rand.NewSource(time.Now().UnixMilli()))
-		for range 10 {
-			numCh <- r.Intn(100 + 1)
+		for range countRandomNum {
+			numCh <- r.Intn(maxRandomNum + 1)
 		}
 		close(numCh)
 	}()
@@ -36,6 +41,8 @@ func main() {
 
 	for square := range squareCh {
 		fmt.Printf("%v ", square)
+		// output:
+		// 6400 7396 169 121 1089 4225 1 4096 7921 16
 	}
 	// wg.Wait() - кажется, что в этом случае необходимости в дополнительной синхронизации нет,
 	// так как горутины синхронизированы каналами

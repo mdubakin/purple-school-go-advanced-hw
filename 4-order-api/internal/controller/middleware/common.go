c626ba8fcaa566a31efef2d35b729ce87894178e
Maksim Dubakin <maxdubakin@yandex.ru>
2025-02-18 08:53:14 +0400

    better logging

--- 4-order-api/internal/controller/middleware/common.go
+++ 4-order-api/internal/controller/middleware/common.go
@@ -7,11 +7,18 @@ type Middleware func(http.Handler) http.Handler
 type WrapperWriter struct {
 	http.ResponseWriter
 	StatusCode int
+	WroteBytes int
 }
 
 func (w *WrapperWriter) WriteHeader(statusCode int) {
 	w.ResponseWriter.WriteHeader(statusCode)
 	w.StatusCode = statusCode
+}
+
+func (w *WrapperWriter) Write(b []byte) (int, error) {
+	n, err := w.ResponseWriter.Write(b)
+	w.WroteBytes += n
+	return n, err
 }
 
 func Chain(middlewares ...Middleware) Middleware {
